b9180a3064e70d6a108c51f7d39308b713f22665
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-03 20:57:26 +0530

    Deck

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Ace of Dimonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
 }
 
 func newCard() string {
