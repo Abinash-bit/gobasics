d45c2b331d1d6bcb850e06582645b30e7bb22972
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-03 22:05:28 +0530

    Receiver function and Creating a deck of cards

--- deck.go
+++ deck.go
@@ -6,6 +6,12 @@ import "fmt"
 // Which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
