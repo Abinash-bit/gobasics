41986b5a761d18efe446a5492f9bba6e35518b12
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-03 17:38:40 +0530

    Quiz

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	card := "Ace of Spades"
+// 	card = "Five of Diamonds"
+// 	fmt.Println(card)
+// }
+
 package main
 
 import "fmt"
 
+var deckSize int
+
 func main() {
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	deckSize = 50
+	fmt.Println(deckSize)
 }
